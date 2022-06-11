71b0a92458bf009ac544f32af98856d119e1b5cc
Varalakshmi G <varalakshmi.ganjam@gmail.com>
2022-06-11 22:37:54 +0530

    adding az and subnets

--- peering.tf
+++ peering.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc_peering_connection" "peer-connection" {
   peer_vpc_id   = aws_vpc.main.id
   vpc_id        = var.DEFAULT_VPC_ID
-  auto_Accept   = true
+  auto_accept   = true
   tags = {
       Name = "peer-from-${var.ENV}-to-default-vpc"
   }
